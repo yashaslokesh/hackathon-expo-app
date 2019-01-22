ef4cd399ca568a59c0bef7a9393a7a41daf38f5b
naveenr414 <dsfan414@gmail.com>
2019-01-22 10:05:13 -0500

    Able to split up challenges into challenge and company

--- scripts/devpost-scraper.py
+++ scripts/devpost-scraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request as ur
 from functools import reduce 
 
-def scrape(link):
+def getChallenges(link):
     sock = ur.urlopen(link)
     soup = BeautifulSoup(sock,"html.parser")
 
@@ -11,30 +11,11 @@ def scrape(link):
     prizeList = filters[0].findAll("label")
     prizeList = [row.contents[-1] for row in prizeList]
 
-    #How many pages of projects are there? 
-    pageInfo = soup.find("div",{"class":"pagination-info"}).find("ul",{"class":"pagination"})
-    maxPage = int(pageInfo.findAll("li")[-2].text)
-
-    allLinks = []
-
-    #Find the link to each of the projects
-    for i in range(1,maxPage+1):
-        linkPage = link+"?page="+str(i)
-        sock = ur.urlopen(linkPage)
-        soup = BeautifulSoup(sock,"html.parser")
-
-        submissionLinks = soup.find("div",{"id":"submission-gallery"})
-        submissionLinks = submissionLinks.findAll("div",{"class":"row"})
-
-        for i in range(len(submissionLinks)):
-            submissionLinks[i] = submissionLinks[i].findAll("a")
-
-        submissionLinks = reduce(lambda x,y: x+y,submissionLinks)
-        submissionLinks = [j['href'] for j in submissionLinks]
-
-        #Add the links found to all links
-        allLinks+=submissionLinks
-
-
+    #Split it up into challenge and company
     
-    return allLinks
+    prizeList = [tuple(row.split(" - ")) for row in prizeList
+                 if len(row.split(" - "))==2]
+    
+    return prizeList
+
+print(getChallenges("https://bitcamp2018.devpost.com/submissions"))
